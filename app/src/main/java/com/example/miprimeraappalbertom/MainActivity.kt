f4082130167c28cf37698d7b33b2061aa0be7717
AlbertoM <albertomunozbautista@gmail.com>
2020-10-14 12:34:12 +0200

    Creación del menú

--- app/src/main/java/com/example/miprimeraappalbertom/MainActivity.kt
+++ app/src/main/java/com/example/miprimeraappalbertom/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.miprimeraappalbertom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    // Creación del menú
+    // https://developer.android.com/guide/topics/ui/menus?hl=es#kotlin
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
 }
